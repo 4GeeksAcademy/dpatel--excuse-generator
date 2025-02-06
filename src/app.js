a0d52d68178d3e4fe6bc0beeaf13865e97fd97bf
22devkpatel <22devkpatel@gmail.com>
2025-02-06 00:10:41 +0000

    message not spaced

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [
+    "My sister ",
+    "My mother ",
+    "My dad ",
+    "My dog ",
+    "My best friend"
+  ];
+  let action = ["crushed", "burned", "yelled at", "chewed up", "puked on"];
+  let what = ["my homework", "my car", "my computer", "my dog", "my phone"];
+  let when = [
+    "last night.",
+    "this morning.",
+    "during vacation.",
+    "during yoga.",
+    "5 minutes ago."
+  ];
+
+  function generateExcuse() {
+    let rdm1 = Math.floor(Math.random() * who.length);
+    let rdm2 = Math.floor(Math.random() * action.length);
+    let rdm3 = Math.floor(Math.random() * what.length);
+    let rdm4 = Math.floor(Math.random() * when.length);
+
+    let excuse = who[rdm1] + action[rdm2] + what[rdm3] + when[rdm4];
+
+    document.getElementById("excuse").innerHTML = excuse;
+  }
+
+  generateExcuse();
 };
